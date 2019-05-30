8627f53ff13df58217fd6852cafb7ee31ec5cce7
田淵健太 <>
2019-05-30 14:58:02 +0900

    I created Entity Class.

---
+++ src/main/java/com/kenta/tabuchi/Student.java
@@ -0,0 +1,74 @@
+package com.kenta.tabuchi;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="M_Student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private Long id;
+	@Column(length=50,nullable=false)
+	private String name;
+	@Column(nullable=true)
+	private Date bithday;
+	@Column(length=18,nullable=true)
+	private String phone;
+	@Column(length=50,nullable=false)
+	private String email;
+	@Column(length=100,nullable=false)
+	private String address;
+	@Column(length=5,nullable=false)
+	private String graduation;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBithday() {
+		return bithday;
+	}
+	public void setBithday(Date bithday) {
+		this.bithday = bithday;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGraduation() {
+		return graduation;
+	}
+	public void setGraduation(String graduation) {
+		this.graduation = graduation;
+	}
+}
