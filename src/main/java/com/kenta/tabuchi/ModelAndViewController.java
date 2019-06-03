1bded6f2dafddf71e22dbdc49407d2eefe4913a8
田淵健太 <>
2019-06-03 20:53:05 +0900

    I insert sample data for test.

--- src/main/java/com/kenta/tabuchi/ModelAndViewController.java
+++ src/main/java/com/kenta/tabuchi/ModelAndViewController.java
@@ -1,5 +1,7 @@
 package com.kenta.tabuchi;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -7,6 +9,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kenta.tabuchi.repositories.StudentRepository;
@@ -17,6 +20,43 @@ public class ModelAndViewController {
 	@Autowired
 	StudentRepository repository;
 	
+	/**
+	 * This is the method that makes some sample data for test.
+	 */
+	@PostConstruct
+	public void init() {
+		Student student1 = new Student();
+		student1.setName("taro yamada");
+		student1.setBirthday("1956/12/10");
+		student1.setPhone("090-1111-2222");
+		student1.setAddress("東京都江東区古谷町一丁目2番3号");
+		student1.setEmail("taro.yamada@google.com");
+		student1.setGraduation("1974");
+		repository.saveAndFlush(student1);
+		
+		Student student2 = new Student();
+		student2.setName("hanako suzuki");
+		student2.setBirthday("1964/05/03");
+		student2.setPhone("080-1234-3456");
+		student2.setAddress("大阪府大阪市都島区柏町５丁目４番２２号");
+		student2.setEmail("suzuki.hanako@exchange.com");
+		student2.setGraduation("1982");
+		repository.saveAndFlush(student2);
+		
+		Student student3 = new Student();
+		student3.setName("hanako yamada");
+		student3.setBirthday("1970/01/30");
+		student3.setPhone("0120-9999-5555");
+		student3.setAddress("北海道旭川市斗亜流町３丁目１０番５号");
+		student3.setEmail("hanako.yamada@yahoo.co.jp");
+		student3.setGraduation("1986");
+		repository.saveAndFlush(student3);
+	}
+	/**
+	 * When user access "index.html" with no post data.This method will invoke.
+	 * @param mav
+	 * @return
+	 */
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
@@ -25,13 +65,43 @@ public class ModelAndViewController {
 		return mav;
 	}
 	
+	/**
+	 * When user push button for find by name.This method will invoke.
+	 * @param mav
+	 * @return
+	 */
+	@RequestMapping(value="/",method=RequestMethod.POST)
+	public ModelAndView post(@RequestParam(value="name")String name, ModelAndView mav) {
+		mav.setViewName("index");
+		System.out.println("post to index invoked");
+		Iterable<Student> list = repository.findByNameLike("%" + name + "%");
+		System.out.println(list.toString());
+		mav.addObject("recordSet", list);
+		return mav;
+	}	
 	
+	
+	/** 
+	 * When user push add_record button,Active page will move there.and this method 
+	 * will invoke like a constructor.
+	 * @param student
+	 * @param mav
+	 * @return
+	 */
 	@RequestMapping(value="/add_record",method=RequestMethod.GET)
 	public ModelAndView addRecord(@ModelAttribute("formModel")Student student,ModelAndView mav) {
 		mav.setViewName("add_record");
 		return mav;
 	}
 	
+	
+	/**
+	 * This method implements validation to add_record form.
+	 * @param student
+	 * @param result
+	 * @param mav
+	 * @return
+	 */
 	@RequestMapping(value="/add_record",method=RequestMethod.POST)
 	public ModelAndView form(
 			@ModelAttribute("formModel") @Validated Student student,
--- src/main/java/com/kenta/tabuchi/repositories/StudentRepository.java
+++ src/main/java/com/kenta/tabuchi/repositories/StudentRepository.java
@@ -1,11 +1,22 @@
 package com.kenta.tabuchi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.kenta.tabuchi.Student;
 
+/**
+ * @author tabuchikenta
+ *
+ *　This is a special interface of SpringFrameWork.
+ *　This interface provide us accessing DB easier.
+ *	When we write method's define in this interface,Compiler implements it's method
+ *	automatically.and the method contains the SQL in it's self with we never see SQL.
+ *	
+ */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+	public List<Student> findByNameLike(String name);
 }
