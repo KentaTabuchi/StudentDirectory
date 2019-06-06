84d4f019a59709ae6a69d3ee11814f89f3e41133
田淵健太 <>
2019-06-06 13:35:01 +0900

    I create part of delete record function that  "find_comfirm.html" ,and also controller arround it too

--- src/main/java/com/kenta/tabuchi/ModelAndViewController.java
+++ src/main/java/com/kenta/tabuchi/ModelAndViewController.java
@@ -1,5 +1,7 @@
 package com.kenta.tabuchi;
 
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,37 +23,11 @@ public class ModelAndViewController {
 	@Autowired
 	StudentRepository repository;
 	
-	/**
-	 * This is the method that makes some sample data for test.
-	 */
+
 	@PostConstruct
 	public void init() {
-		Student student1 = new Student();
-		student1.setName("山田 太郎");
-		student1.setBirthday("1956/12/10");
-		student1.setPhone("090-1111-2222");
-		student1.setAddress("東京都江東区古谷町一丁目2番3号");
-		student1.setEmail("taro.yamada@google.com");
-		student1.setGraduation("1974");
-		repository.saveAndFlush(student1);
-		
-		Student student2 = new Student();
-		student2.setName("鈴木 花子");
-		student2.setBirthday("1964/05/03");
-		student2.setPhone("080-1234-3456");
-		student2.setAddress("大阪府大阪市都島区柏町５丁目４番２２号");
-		student2.setEmail("suzuki.hanako@exchange.com");
-		student2.setGraduation("1982");
-		repository.saveAndFlush(student2);
-		
-		Student student3 = new Student();
-		student3.setName("山田 花子");
-		student3.setBirthday("1970/01/30");
-		student3.setPhone("0120-9999-5555");
-		student3.setAddress("北海道旭川市斗亜流町３丁目１０番５号");
-		student3.setEmail("hanako.yamada@yahoo.co.jp");
-		student3.setGraduation("1986");
-		repository.saveAndFlush(student3);
+		TestData testData = new TestData();
+		testData.setSampleDataToDB(repository);
 	}
 	/**
 	 * When user access "index.html" with no post data.This method will invoke.
@@ -139,6 +115,32 @@ public class ModelAndViewController {
 		mav.setViewName("find_record");
 		Iterable<Student> list = repository.findByNameLike("%"+name+"%");
 		mav.addObject("recordSet",list);
+		return mav;
+	}
+	@RequestMapping(value="/delete_record",method=RequestMethod.GET)
+	public ModelAndView deleteRecord(
+			ModelAndView mav) {
+		mav.setViewName("delete_record");
+		Iterable<Student> list = repository.findAll();
+		mav.addObject("recordSet", list);
+		return mav;
+	}
+	@RequestMapping(value="/delete_record",method=RequestMethod.POST)
+	public ModelAndView deleteRecordPost(
+			@RequestParam("name")String name,
+			ModelAndView mav) {
+		mav.setViewName("delete_record");
+		Iterable<Student> list = repository.findByNameLike("%"+name+"%");
+		mav.addObject("recordSet",list);
+		return mav;
+	}
+	@RequestMapping(value="/delete_comfirm",method=RequestMethod.POST)
+	public ModelAndView deleteComfirm(
+			@RequestParam("id")String id,
+			ModelAndView mav) {
+		mav.setViewName("delete_comfirm");
+		Optional<Student> record = repository.findById(Long.valueOf(id));
+		mav.addObject("formModel",record.get());
 		return mav;
 	}
 }
